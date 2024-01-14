80ae31e5b2002903e16811f0574887f156c2ae2f
caption01 <nutchapon.han@gmail.com>
2024-01-14 21:16:54 +0700

    add nginx config

--- ping.sh
+++ ping.sh
@@ -4,3 +4,8 @@
 ansible all -i <public-ip>, \
     -m ping \
     -e "ansible_user=<username> ansible_password=<password> ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
+
+
+ansible all -i ec2-18-136-98-145.ap-southeast-1.compute.amazonaws.com, \
+    -m ping \
+    -e "ansible_user=ansible ansible_password=admin1234 ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
